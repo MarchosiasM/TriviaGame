f0526265c4a4846b995523d4cf46c5e6d5008131
Michael Malach <mmalach@ucsd.edu>
2018-03-02 20:10:32 -0800

    First push, just to get something up

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+// Initialize everything, going to need an array to store the questions and answers. 
+// Perhaps an array to store daughter arrays, and the first entry will be the question, 
+// second will be the answer, and trailing will be guesses.
+
