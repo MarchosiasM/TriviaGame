f74e98352c8f90ec8cde210362c0c4ba2012b07f
Michael Malach <mmalach@ucsd.edu>
2018-03-03 08:16:10 -0800

    Small update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,3 +2,27 @@
 // Perhaps an array to store daughter arrays, and the first entry will be the question, 
 // second will be the answer, and trailing will be guesses.
 
+// One master array to store the others for reference later.
+
+var arrayMaster = [
+    q1
+]
+
+// Child arrays will encapsulate the data we need for the questions
+
+var q1 = [
+    "What about the one guy", //0th entry is question
+    "He's cool I guess", //1st is answer
+    "He is cool I guess", // Begin listing 4 possible answers
+    "What a nerd",
+    "That guy loves apples",
+    "That's not a real answer"
+]
+
+// Now you're gonna need some code what makes the game do a thing.
+// 
+
+$(document).ready(function () {
+  
+    
+});
